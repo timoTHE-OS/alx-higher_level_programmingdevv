e2f0468d0d65c7fd46f591a9a3407b0452f13d68
lroudge <lauradrc@hotmail.fr>
2019-04-29 19:48:32 +0000

    Correction of two tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,10 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (fast)
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
