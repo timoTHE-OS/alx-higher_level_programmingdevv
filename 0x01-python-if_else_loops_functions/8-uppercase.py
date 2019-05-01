cdfee42cb8bc1c63f38f121a3198e41ac1124411
lroudge <lauradrc@hotmail.fr>
2019-05-01 00:05:28 +0000

    Correction of task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upper = ''
     for c in str:
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            upper += chr(ord(c) - 32)
+            char = chr(ord(c) - 32)
         else:
-            upper += c
-    print(upper)
+            char = c
+        print("{:s}".format(char), end="")
+    print('')
