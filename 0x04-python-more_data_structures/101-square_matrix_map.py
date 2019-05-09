cef574c87f6f1ba4debedd681d19946d5d531947
lroudge <lauradrc@hotmail.fr>
2019-05-09 22:40:51 +0000

    Correction of advanced task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: [i ** 2 for i in x], matrix))
+    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
