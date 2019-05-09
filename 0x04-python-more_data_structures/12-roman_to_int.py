85f6d0398f221b76555c7006c556451ad8c90e23
lroudge <lauradrc@hotmail.fr>
2019-05-09 21:51:03 +0000

    Pep8 on task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,8 @@ def roman_to_int(roman_string):
     roman_n = 0
     for j in range(len(roman_string)):
         if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_d[roman_string[j]] - 2 * roman_d[roman_string[j - 1]]
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
             roman_n += roman_d[roman_string[j]]
     return roman_n
