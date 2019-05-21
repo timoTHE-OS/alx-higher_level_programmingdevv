a4152feb6e37b621fbca40c49ccf5a8dd3aed1bc
lroudge <lauradrc@hotmail.fr>
2019-05-21 21:38:15 +0000

    Add advanced task 10

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class MagicClass:
+    """Initialization of the MagicClass.
+    Private methode __init__(self, radius=0)
+    Public method area(self).
+    Public method circumference(self).
+    """
+    def __init__(self, radius):
+        """Initialization of the data."""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Calculation of the area."""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculation of the circumference."""
+        return 2 * math.pi * self.__radius
