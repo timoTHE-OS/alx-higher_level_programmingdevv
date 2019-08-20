109aa11c252d3d899d5827f3e2a24e451c48dd7a
lroudge <lauradrc@hotmail.fr>
2019-08-20 21:08:21 +0000

    Refactor 6 and boil it down to essential checks for efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,20 +5,18 @@
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
 
-    l = list_of_integers.copy()
-    if l is None or l == []:
+    if list_of_integers is None or list_of_integers == []:
         return None
-    if len(set(l)) == 1:
-        return l[0]
     lo = 0
-    hi = len(l)
-    mid = ((hi - lo) / 2) + lo
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
     mid = int(mid)
     if hi == 2:
-        return max(l)
-    if l[mid] > l[mid - 1] and l[mid] > l[mid + 1]:
-        return l[mid]
-    if mid > 0 and l[mid] < l[mid + 1]:
-        return find_peak(l[mid + 1:])
-    if mid > 0 and l[mid] < l[mid - 1]:
-        return find_peak(l[:mid])
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1]
+    and list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid + 1:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
