5e8436c1b87ea60a05e889dd96dbbc0c6e8f83d7
lroudge <lauradrc@hotmail.fr>
2019-08-20 19:37:43 +0000

    Correction of task 6 and 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -s "$1" -o /dev/null -w "%{response_code}"
+curl -sI -w "%{response_code}" "$1" -o /dev/null
