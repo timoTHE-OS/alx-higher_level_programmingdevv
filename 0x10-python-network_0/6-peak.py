d1bcc5a5029e8465f7d3c499d0c61ed839395c4d
lroudge <lauradrc@hotmail.fr>
2019-08-20 21:18:36 +0000

    Style on task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,8 +13,8 @@ def find_peak(list_of_integers):
     mid = int(mid)
     if hi == 2:
         return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and
-    list_of_integers[mid] >= list_of_integers[mid + 1]:
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid + 1:])
