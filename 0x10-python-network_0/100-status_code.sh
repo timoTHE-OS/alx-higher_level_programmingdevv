56c1812554b87826e8a62a2381d280b69a31c6f7
lroudge <lauradrc@hotmail.fr>
2019-08-20 19:55:13 +0000

    Make advanced tasks executable...

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -s --head --write-out '%{http_code}' "$1" -o /dev/null
+curl -sI -w '%{response_code}' "$1" -o /dev/null
