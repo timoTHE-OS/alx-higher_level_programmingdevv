d3ee0d3b471f9001e80656f9cc1657888f6c5129
lroudge <lauradrc@hotmail.fr>
2019-08-20 19:44:30 +0000

    New correction of task 6 and 7, correction of task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2"
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
+
