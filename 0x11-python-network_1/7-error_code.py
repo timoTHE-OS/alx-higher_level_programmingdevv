35c8b17185ae3b4efc66efe3ffcb1a0242e255b0
lroudge <lauradrc@hotmail.fr>
2019-08-26 20:34:39 +0000

    Correction of task 7 and 9

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     r = requests.get(sys.argv[1])
     code = r.status_code
-    if code > 400:
-        print("Error code: {}".format(code))
+    if code >= 400:
+        print("Error code: " + str(code))
     else:
         print(r.text)
--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
 
     r = requests.get('https://swapi.co/api/people/?search={}'
-                     .format(sys.argv[1]))
-    print('Number of results: {}'.format(r.json().get('count')))
-    for res in r.json().get('results'):
+                     .format(sys.argv[1])).json()
+    print('Number of results: {}'.format(r.get('count')))
+    for res in r.get('results'):
         print(res.get('name'))
