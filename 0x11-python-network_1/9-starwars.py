387712ba16841af91ba9d8908ffe36e480db5af2
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:42:45 +0000

    Correction of task 9

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Takes in a string and sends a search request to the Star Wars API"""
 
+if __name__ == "__main__":
+    import requests
+    import sys
 
-import requests
-import sys
-
-r = requests.get('https://swapi.co/api/people/?search={}'.format(sys.argv[1]))
-print('Number of results: {}'.format(r.json()['count']))
-for res in r.json()['results']:
-    print(res['name'])
+    r = requests.get('https://swapi.co/api/people/?search={}'
+                     .format(sys.argv[1]))
+    print('Number of results: {}'.format(r.json().get('count')))
+    for res in r.json().get('results'):
+        print(res.get('name'))
