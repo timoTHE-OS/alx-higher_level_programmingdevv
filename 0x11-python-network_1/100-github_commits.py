98829d0486d6582d8fce0ec78427f97988311913
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:50:06 +0000

    Correction of task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,13 +3,14 @@
 10 commits (from the most recent to oldest)"""
 
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                 .format(sys.argv[2], sys.argv[1]))
-if r.status_code >= 400:
-    print('None')
-else:
-    for com in r.json()[:10]:
-        print("{}: {}".format(com['sha'], com['author']['login']))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'), com.get('author').get('login')))
