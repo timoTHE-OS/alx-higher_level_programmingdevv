e44175161d936f35dde9023d3444738b140ee411
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:17:07 +0000

    Correction of task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,13 @@
 """Takes in a URL, sends a request to the URL and
 displays the body of the response"""
 
-import urllib.request
-from urllib.error import HTTPError
-import sys
+if __name__ == "__main__":
+    import urllib.request
+    from urllib.error import HTTPError
+    import sys
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode("utf-8", "replace"))
-except HTTPError as e:
-    print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8", "replace"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
