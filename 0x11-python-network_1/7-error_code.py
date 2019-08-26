daa5b23daf28025e396392184637369d36f6c9f5
lroudge <lauradrc@hotmail.fr>
2019-08-26 21:02:59 +0000

    Correction of task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,14 +2,14 @@
 """Takes in a URL, sends a request to the URL and
 displays the body of the response"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    import requests
-    import sys
 
     r = requests.get(sys.argv[1])
     code = r.status_code
     if code >= 400:
-        print("Error code: " + str(code))
+        print("Error code: {}".format(code))
     else:
         print(r.text)
