a3e2eac4245b43df142f7ce455dd2912f179d8a8
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:46:52 +0000

    Correction of task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,12 +3,13 @@
 to display an id"""
 
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-r = requests.get('https://api.github.com/user',
-                 auth=(sys.argv[1], sys.argv[2]))
-if r.status_code >= 400:
-    print('None')
-else:
-    print(r.json()['id'])
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        print(r.json().get('id'))
