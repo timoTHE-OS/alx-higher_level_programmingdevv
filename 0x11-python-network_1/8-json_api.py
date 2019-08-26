fc67285b6201d559fba36ad761821d997ba4359c
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:31:30 +0000

    Correction of task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,16 +2,18 @@
 """Takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter"""
 
-import requests
-import sys
 
-q = sys.argv[1] if len(sys.argv) > 1 else ""
-r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-if r.json() == {}:
-    print('No result')
-else:
-    try:
-        dic = r.json()
-        print("[{}] {}".format(dic['id'], dic['name']))
-    except ValueError:
-        print('Not a valid JSON')
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    if r.json() == {}:
+        print('No result')
+    else:
+        try:
+            dic = r.json()
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+        except ValueError:
+            print('Not a valid JSON')
