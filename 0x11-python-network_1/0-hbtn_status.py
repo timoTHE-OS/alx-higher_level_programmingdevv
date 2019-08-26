43167bcf3f25cb576dd8f2603ac56410c45b133e
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:08:02 +0000

    Correction of task 0 and 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """Fetches https://intranet.hbtn.io/status"""
 
 
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    print('    - type: {}'.format(type(html)))
-    print('    - content: {}'.format(html))
-    print('    - utf8 content: {}'.format(html.decode("utf-8", "replace")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('    - type: {}'.format(type(html)))
+        print('    - content: {}'.format(html))
+        print('    - utf8 content: {}'.format(html.decode("utf-8", "replace")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,10 +3,12 @@
 X-Request-Id variable found in the header of the response"""
 
 
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    head = response.info().__dict__
-    header = [x[1] for x in head['_headers'] if x[0] == 'X-Request-Id']
-    print(header[0])
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.info().__dict__
+        headers = head.get('_headers')
+        h = [x[1] for x in headers if x[0] == 'X-Request-Id']
+        print(h[0])
