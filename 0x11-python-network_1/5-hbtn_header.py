3d51ff8f62f33f037c41782d17b6649d510e99dd
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:22:20 +0000

    Correction of task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,8 +3,9 @@
 X-Request-Id variable found in the header of the response"""
 
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-r = requests.get(sys.argv[1])
-print(r.headers['X-Request-Id'])
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
