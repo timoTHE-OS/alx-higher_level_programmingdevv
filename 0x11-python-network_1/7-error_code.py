a1f77b22a063297f2809d5941578a22fa2db95fa
lroudge <lauradrc@hotmail.fr>
2019-08-26 19:35:07 +0000

    Correction of task 7 and 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import sys
 
     r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
+    if r.status_code > 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
 
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-    if r.json() == {}:
-        print('No result')
-    else:
-        try:
-            dic = r.json()
+    try:
+        dic = r.json()
+        if dic == {}:
+            print('No result')
+        else:
             print("[{}] {}".format(dic.get('id'), dic.get('name')))
-        except ValueError:
-            print('Not a valid JSON')
+    except ValueError:
+        print('Not a valid JSON')
