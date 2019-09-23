f54c412b731c71f0d54056e0c2b449c3ff55b4c8
lroudge <lauradrc@hotmail.fr>
2019-09-23 22:34:03 +0000

    Add advanced tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const values = [...new Set(Object.keys(dict).map(function (key) { return dict[key]; }))];
+const keys = Object.keys(dict);
+const newDict = {};
+
+for (let i = 0; i < 3; i++) {
+  newDict[values[i]] = keys.map(function (x) { if (dict[x] === values[i]) { return (x); } }).filter(Boolean);
+}
+
+console.log(newDict);
