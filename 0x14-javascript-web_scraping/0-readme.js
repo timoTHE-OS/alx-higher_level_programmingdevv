7a43308d079fd9bb14d05290ea16823595fe0b84
lroudge <lauradrc@hotmail.fr>
2019-09-24 19:12:42 +0000

    Try 0 with Object()

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,5 +6,5 @@ try {
   const content = fs.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log(err); });
   console.log(content);
 } catch (err) {
-  console.log(err);
+  console.log(Object(err));
 }
