fb6bc396fd9139d8342d1d1a8ba17797a44a1ddd
lroudge <lauradrc@hotmail.fr>
2019-09-24 21:18:21 +0000

    Add utf-8

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 try {
-  const content = fs.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log(err); });
+  const content = fs.readFileSync(process.argv[2], 'utf-8', function (err, result) { if (err) console.log(err); });
   console.log(content);
 } catch (err) {
   console.log(err);
