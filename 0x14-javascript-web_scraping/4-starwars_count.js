d839fa26c788a9d8ebd76f10f1ea3c445956e2f0
lroudge <lauradrc@hotmail.fr>
2019-09-24 17:18:35 +0000

    Modify task 4 and add tasks 5 and 6

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,11 +6,15 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
-  const nb = JSON.parse(body).results.reduce((acc, elem) => {
+  /* const nb = JSON.parse(body).results.reduce((acc, elem) => {
     acc += elem.characters.reduce((acc, character) => {
       return (character === 'https://swapi.co/api/people/18/' ? acc + 1 : acc);
     }, 0);
     return (acc);
   }, 0);
+  */
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.includes('https://swapi.co/api/people/18/');
+  }).length;
   console.log(nb);
 });
