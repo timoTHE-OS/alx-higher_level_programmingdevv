e3ca32d48d0ea8298888d30ce8f8bcab20fc3e3b
lroudge <lauradrc@hotmail.fr>
2019-09-26 20:15:38 +0000

    Correction of task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,3 @@
-const lang = $('html').attr('lang');
-$.get('https://fourtonfish.com/hellosalut/?lang=' + lang, function (data) {
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
   $('DIV#hello').text(data.hello);
 });
