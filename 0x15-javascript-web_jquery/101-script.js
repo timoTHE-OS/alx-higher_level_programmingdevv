94b47efb4074038fd95136dcb34135d3c084350d
lroudge <lauradrc@hotmail.fr>
2019-09-26 19:46:45 +0000

    Modification in task 11

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', function () {
-  $("DIV#add_item").click(function () {
-    $("<li>Item</li>").appendTo("UL.my_list");
+  $('DIV#add_item').click(function () {
+    $('<li>Item</li>').appendTo('UL.my_list');
   });
-  $("DIV#remove_item").click(function () {
-    $(".my_list li").last().remove();
+  $('DIV#remove_item').click(function () {
+    $('.my_list li').last().remove();
   });
-  $("DIV#clear_list").click(function () {
-    $(".my_list li").map(function () {
+  $('DIV#clear_list').click(function () {
+    $('.my_list li').map(function () {
       this.remove();
     });
   });
